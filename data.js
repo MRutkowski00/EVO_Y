75ce0b65ca608e4fd6936fdd66e281638a5ab38a
MRutkowski00 <m.rutkowski.archviz@gmail.com>
2024-09-27 19:21:35 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,410 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-komunikacja",
+      "name": "Komunikacja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.023931537840441663,
+          "pitch": 0.08100683715567669,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": 0.3862355478162165,
+          "pitch": 0.06495516880339913,
+          "rotation": 0,
+          "target": "3-azienka"
+        },
+        {
+          "yaw": 2.705391914992271,
+          "pitch": 0.0727278964421938,
+          "rotation": 0,
+          "target": "5-sypialnia"
+        },
+        {
+          "yaw": -3.086502755412038,
+          "pitch": 0.06702519138272933,
+          "rotation": 0,
+          "target": "4-wc"
+        },
+        {
+          "yaw": -2.774100488405727,
+          "pitch": 0.059860483899313977,
+          "rotation": 0,
+          "target": "6-pokj-a"
+        },
+        {
+          "yaw": -1.2512001948470086,
+          "pitch": 0.10747346176999173,
+          "rotation": 0,
+          "target": "7-pokj-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3911923661694061,
+          "pitch": 0.05831554780077575,
+          "title": "Wiatrołap",
+          "text": "dostęp do:<div><br></div><div>- pomieszczenia gospodarczego&nbsp;</div><div>- pomieszczenia technicznego&nbsp;</div>"
+        },
+        {
+          "yaw": 1.4760179673174365,
+          "pitch": 0.1291272960921539,
+          "title": "Pralnia",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0463023120588684,
+          "pitch": 0.09010985398640159,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        },
+        {
+          "yaw": -0.7642695010950042,
+          "pitch": 0.06677998562226861,
+          "rotation": 0,
+          "target": "2-aneks-kuchenny"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aneks-kuchenny",
+      "name": "Aneks kuchenny",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6066591774831203,
+          "pitch": 0.09376846732784472,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.005143389850585933,
+          "pitch": 0.041123735671448,
+          "title": "Spiżarnia",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.480527199730913,
+          "pitch": 0.055464000761947574,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8145733204944126,
+          "pitch": 0.07243548789764809,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.676741670655379,
+          "pitch": 0.06470556656491233,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1437643535010693,
+          "pitch": 0.1319583361644785,
+          "title": "Garderoba",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "6-pokj-a",
+      "name": "Pokój A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5443875135002214,
+          "pitch": 0.06087838069458762,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pokj-b",
+      "name": "Pokój B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.831640864126472,
+          "pitch": 0.09383499767042736,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "EVO Y",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
